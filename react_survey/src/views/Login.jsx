7e16ea3cf35485e4ba9bb92111ac28c28225b302
iren <irasibil2@gmail.com>
2023-03-03 10:13:25 +0200

    implement login functionality

--- react_survey/src/views/Login.jsx
+++ react_survey/src/views/Login.jsx
@@ -1,6 +1,37 @@
 import { LockClosedIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axiosClient from '../axios';
+import { useStateContext } from '../contexts/ContextProvider';
 
 export default function Login() {
+    const { setCurrentUser, setUserToken } = useStateContext();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState({__html: ''});
+
+    const onSubmit = e => {
+        e.preventDefault();
+        setError({__html: ''});
+
+        axiosClient.post('/login', {
+            email,
+            password,
+        })
+        .then(({data}) => {
+            setCurrentUser(data.user);
+            setUserToken(data.token);
+        })
+        .catch((error) => {
+            if (error.response) {
+                const finalErrors = Object.values(error.response.data.errors).reduce((accum, next) => [...accum, ...next], []);
+                console.log(finalErrors);
+                setError({__html: finalErrors.join('<br>')});
+            }
+            console.log("error", error);
+        })
+    }
+
     return (
         <>
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
@@ -8,11 +39,14 @@ export default function Login() {
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
                 Or{' '}
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                    start your 14-day free trial
-                </a>
+                <Link to="/signup" className="font-medium text-indigo-600 hover:text-indigo-500">
+                    signup for free
+                </Link>
             </p>
-            <form className="mt-8 space-y-6" action="#" method="POST">
+
+            {error.__html && (<div className='bg-red-500 rounded py-2 px-3 text-white' dangerouslySetInnerHTML={error}></div>)}
+
+            <form onSubmit={onSubmit} className="mt-8 space-y-6" action="#" method="POST">
                 <input type="hidden" name="remember" defaultValue="true" />
                 <div className="-space-y-px rounded-md shadow-sm">
                     <div>
@@ -25,6 +59,8 @@ export default function Login() {
                             type="email"
                             autoComplete="email"
                             required
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
                             className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                             placeholder="Email address"
                         />
@@ -39,6 +75,8 @@ export default function Login() {
                             type="password"
                             autoComplete="current-password"
                             required
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
                             className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                             placeholder="Password"
                         />
@@ -56,12 +94,6 @@ export default function Login() {
                         <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
                             Remember me
                         </label>
-                    </div>
-
-                    <div className="text-sm">
-                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                            Forgot your password?
-                        </a>
                     </div>
                 </div>
 
